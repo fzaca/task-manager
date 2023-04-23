c488406bdfc94b9b22633e3592f8fa1008802948
Xukay101 <zaca03zaca@gmail.com>
2023-04-23 19:35:41 -0300

    Update ui

--- controllers/main_window.py
+++ controllers/main_window.py
@@ -1,13 +1,11 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QMainWindow
 
 from views.main_window_ui import Ui_MainWindow
 
-class MainWindowForm(QWidget, Ui_MainWindow):
+class MainWindowForm(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
-        self.test_button.clicked.connect(self.hello)
 
     def hello(self):
         print('Hello World')
--- views/main_window_ui.py
+++ views/main_window_ui.py
@@ -15,19 +15,48 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
+    QListWidget, QListWidgetItem, QMainWindow, QPushButton,
+    QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(450, 266)
+        MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setGeometry(QRect(0, 0, 431, 258))
-        self.test_button = QPushButton(self.centralwidget)
-        self.test_button.setObjectName(u"test_button")
-        self.test_button.setGeometry(QRect(180, 110, 93, 28))
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.task_list = QListWidget(self.centralwidget)
+        self.task_list.setObjectName(u"task_list")
+
+        self.verticalLayout.addWidget(self.task_list)
+
+        self.task_input_container = QWidget(self.centralwidget)
+        self.task_input_container.setObjectName(u"task_input_container")
+        self.horizontalLayout = QHBoxLayout(self.task_input_container)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.task_label = QLabel(self.task_input_container)
+        self.task_label.setObjectName(u"task_label")
+
+        self.horizontalLayout.addWidget(self.task_label)
+
+        self.task_input = QLineEdit(self.task_input_container)
+        self.task_input.setObjectName(u"task_input")
+
+        self.horizontalLayout.addWidget(self.task_input)
+
+        self.add_button = QPushButton(self.task_input_container)
+        self.add_button.setObjectName(u"add_button")
+
+        self.horizontalLayout.addWidget(self.add_button)
+
+
+        self.verticalLayout.addWidget(self.task_input_container)
+
+        MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
 
@@ -35,7 +64,8 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Codificador/Decodificador", None))
-        self.test_button.setText(QCoreApplication.translate("MainWindow", u"Test", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Task Manager", None))
+        self.task_label.setText(QCoreApplication.translate("MainWindow", u"Task:", None))
+        self.add_button.setText(QCoreApplication.translate("MainWindow", u"Add", None))
     # retranslateUi
 
