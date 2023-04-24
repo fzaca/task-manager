e102201f671b0f8e54d9694591c6a2cf5dfda2cf
Xukay101 <zaca03zaca@gmail.com>
2023-04-23 22:03:20 -0300

    Update ui

--- views/main_window_ui.py
+++ views/main_window_ui.py
@@ -15,46 +15,68 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
-    QListWidget, QListWidgetItem, QMainWindow, QPushButton,
-    QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLineEdit, QMainWindow,
+    QPushButton, QScrollArea, QSizePolicy, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet(u"background-color: #3c3836;")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.task_list = QListWidget(self.centralwidget)
-        self.task_list.setObjectName(u"task_list")
+        self.scroll_area = QScrollArea(self.centralwidget)
+        self.scroll_area.setObjectName(u"scroll_area")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.scroll_area.sizePolicy().hasHeightForWidth())
+        self.scroll_area.setSizePolicy(sizePolicy)
+        self.scroll_area.setStyleSheet(u"border: 0px;")
+        self.scroll_area.setLineWidth(0)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 782, 536))
+        self.tasks_layout = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.tasks_layout.setSpacing(6)
+        self.tasks_layout.setObjectName(u"tasks_layout")
+        self.tasks_layout.setContentsMargins(0, 0, 0, 0)
+        self.scroll_area.setWidget(self.scrollAreaWidgetContents)
 
-        self.verticalLayout.addWidget(self.task_list)
+        self.verticalLayout.addWidget(self.scroll_area)
 
-        self.task_input_container = QWidget(self.centralwidget)
-        self.task_input_container.setObjectName(u"task_input_container")
-        self.horizontalLayout = QHBoxLayout(self.task_input_container)
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setStyleSheet(u"background-color: #d3869b;\n"
+"border-radius: 5px;")
+        self.horizontalLayout = QHBoxLayout(self.widget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.task_label = QLabel(self.task_input_container)
-        self.task_label.setObjectName(u"task_label")
-
-        self.horizontalLayout.addWidget(self.task_label)
-
-        self.task_input = QLineEdit(self.task_input_container)
+        self.horizontalLayout.setContentsMargins(5, 5, 5, 5)
+        self.task_input = QLineEdit(self.widget)
         self.task_input.setObjectName(u"task_input")
 
         self.horizontalLayout.addWidget(self.task_input)
 
-        self.add_button = QPushButton(self.task_input_container)
+        self.add_button = QPushButton(self.widget)
         self.add_button.setObjectName(u"add_button")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.add_button.sizePolicy().hasHeightForWidth())
+        self.add_button.setSizePolicy(sizePolicy1)
+        self.add_button.setMinimumSize(QSize(70, 30))
+        self.add_button.setStyleSheet(u"background-color: #b8bb36; border-radius: 5px;")
 
         self.horizontalLayout.addWidget(self.add_button)
 
 
-        self.verticalLayout.addWidget(self.task_input_container)
+        self.verticalLayout.addWidget(self.widget)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -65,7 +87,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Task Manager", None))
-        self.task_label.setText(QCoreApplication.translate("MainWindow", u"Task:", None))
+        self.task_input.setInputMask("")
+        self.task_input.setText("")
+        self.task_input.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Task", None))
         self.add_button.setText(QCoreApplication.translate("MainWindow", u"Add", None))
     # retranslateUi
 
